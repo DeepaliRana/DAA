3ed3409f3878b2550cc09db0e987bb9dbfb0cce0
DeepaliRana <143646582+DeepaliRana@users.noreply.github.com>
2023-09-12 20:32:46 +0530

    Add files via upload

---
+++ Hollow rhombus.cpp
@@ -0,0 +1,26 @@
+// Hollow rohmbus
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= n; j++) {
+            if (i == 1 || i == n || j == 1 || j == n) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
