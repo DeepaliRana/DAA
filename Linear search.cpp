40f36ffe93ff44fa78fc6aab7ef17b29737e0983
DeepaliRana <143646582+DeepaliRana@users.noreply.github.com>
2023-09-12 20:34:30 +0530

    Add files via upload

---
+++ Linear search.cpp
@@ -0,0 +1,38 @@
+// linear search 
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+int main() {
+    int n, key;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search for: ");
+    scanf("%d", &key);
+
+    int index = linearSearch(arr, n, key);
+
+    if (index != -1) {
+        printf("Element %d found at index %d.\n", key, index);
+    } else {
+        printf("Element %d not found in the array.\n", key);
+    }
+
+    return 0;
+}
+
